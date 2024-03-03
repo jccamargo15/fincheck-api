2145d02dc24bc7a524a69c7b2b818e957d132594
José Carlos de Camargo <jccamargo15@gmail.com>
2024-03-02 21:56:39 -0300

    feat: generate jwt token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      signOptions: { expiresIn: '7d' },
+      secret: 'unsecure_jwt_secret',
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })

8b59d7bf863cddfc09e3f6a717096f23a936c04f
José Carlos de Camargo <jccamargo15@gmail.com>
2024-03-13 23:44:56 -0300

    feat: database url validation

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -4,11 +4,16 @@ import { IsNotEmpty, IsString, NotEquals, validateSync } from 'class-validator';
 class Env {
   @IsString()
   @IsNotEmpty()
+  dbURL: string;
+
+  @IsString()
+  @IsNotEmpty()
   @NotEquals('unsecure_jwt_secret')
   jwtSecret: string;
 }
 
 export const env: Env = plainToInstance(Env, {
+  dbURL: process.env.DATABASE_URL,
   jwtSecret: process.env.JWT_SECRET,
 });
 

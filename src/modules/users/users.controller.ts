2ddf63de8ab7fa938ac20df3735c4e5c793a45f8
José Carlos de Camargo <jccamargo15@gmail.com>
2024-03-14 23:57:23 -0300

    feat: add me route

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  @UseGuards(AuthGuard)
+  me(@Req() request: any) {
+    return this.usersService.getUserById('userId');
+  }
 }

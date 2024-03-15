55fd908e3cc78ba116b77ca719b6fa5733f0a7cf
José Carlos de Camargo <jccamargo15@gmail.com>
2024-03-15 00:10:02 -0300

    feat: setup global guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  @UseGuards(AuthGuard)
   me(@Req() request: any) {
+    console.log({ meuUserIe: request.userId });
+
     return this.usersService.getUserById('userId');
   }
 }

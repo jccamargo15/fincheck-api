1f998dc10f08af20056444c371231fac0f10400d
José Carlos de Camargo <jccamargo15@gmail.com>
2024-03-27 11:35:53 -0300

    fix: export ValidateBankAccountOwnershipService

--- src/modules/transactions/services/transactions.service.ts
+++ src/modules/transactions/services/transactions.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
 import { CreateTransactionDto } from '../dto/create-transaction.dto';
 import { UpdateTransactionDto } from '../dto/update-transaction.dto';
-import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
-import { ValidateBankAccountOwnershipService } from '../../bank-accounts/services/validate-bank-account-ownership.service';
-import { ValidateCategoryOwnershipService } from '../../categories/services/validate-category-ownership.service';
-import { ValidateTransactionOwnershipService } from './validate-transaction-ownership.service';
 import { TransactionType } from '../entities/Transaction';
+import { ValidateTransactionOwnershipService } from './validate-transaction-ownership.service';
+import { ValidateBankAccountOwnershipService } from 'src/modules/bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnershipService } from 'src/modules/categories/services/validate-category-ownership.service';
 
 @Injectable()
 export class TransactionsService {
